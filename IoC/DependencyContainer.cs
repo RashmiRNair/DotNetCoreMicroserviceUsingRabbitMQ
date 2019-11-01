129e6fee080281c2ebe4b92e31483fd6e452444f
Rashmi_Nair <rashmi_nair@symantec.com>
2019-11-01 15:11:10 +0530

    Added dependency injection class

---
+++ IoC/DependencyContainer.cs
@@ -0,0 +1,17 @@
+﻿using Core.Bus;
+using Messaging;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IoC
+{
+   public static class DependencyContainer
+    {
+        public static void RegisterServices(IServiceCollection services)
+        {
+            services.AddTransient<IEventBus, RabbitMQBus>();
+        }
+    }
+}
